53bcad813dc280e925aa1053f59936e45c99ad4e
Narthana Epa <narthana.epa@gmail.com>
2024-08-27 09:10:18 +0530

    Add comment and implement Display for Response

--- src/response.rs
+++ src/response.rs
@@ -1,8 +1,29 @@
+use std::fmt::{self, Display, Formatter};
+
 #[derive(Debug, PartialEq, Eq)]
 pub enum Response {
-    Ok,
-    Err(String),
+    Ok(Option<String>),
+    Err(u64, String),
     D(String),
+    Comment(String),
     S(String, String),
     Inquire(String, String),
+}
+
+impl Display for Response {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        use Response::*;
+        match self {
+            Ok(s) => write!(
+                f,
+                "OK{}",
+                s.as_ref().map(|s| format!(" {}", s)).unwrap_or_default(),
+            ),
+            Err(code, msg) => write!(f, "ERR {} {}", code, msg),
+            D(s) => write!(f, "D {}", s),
+            Comment(s) => write!(f, "# {}", s),
+            S(k, v) => write!(f, "S {} {}", k, v),
+            Inquire(k, v) => write!(f, "INQUIRE {} {}", k, v),
+        }
+    }
 }
